d69f150903b6f3472c871197ba16a0e070f359d3
Su Min Kim <suminkim@Sus-MacBook-Air.home>
2020-07-29 00:18:46 -0400

    Updates

---
+++ precache-manifest.d8fed6909b24678fa4d185ea4e96aee3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26fa38908ec3279e3b1fefd5d392587b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e28759c05cca1faee315",
+    "url": "/static/css/2.7550f3b0.chunk.css"
+  },
+  {
+    "revision": "9166e624127b36ae575f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e28759c05cca1faee315",
+    "url": "/static/js/2.8120e4f1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8120e4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9166e624127b36ae575f",
+    "url": "/static/js/main.b3eeecd7.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "a59e259534a84822424c3f25c39af83f",
+    "url": "/static/media/Bloom.a59e2595.png"
+  },
+  {
+    "revision": "4c6dd91fd493248b43d461ec42ff6647",
+    "url": "/static/media/Emotinews.4c6dd91f.png"
+  },
+  {
+    "revision": "aace551cf8349b611358ddf64f53c0c9",
+    "url": "/static/media/RecipeBook.aace551c.png"
+  },
+  {
+    "revision": "20f6b6cdbb50751ea883df1764bb4de7",
+    "url": "/static/media/Su Min Kim Resume.20f6b6cd.pdf"
+  },
+  {
+    "revision": "e5f22853e6d82186b15e73199b16e709",
+    "url": "/static/media/socialTrackerMainPic.e5f22853.png"
+  }
+]);
